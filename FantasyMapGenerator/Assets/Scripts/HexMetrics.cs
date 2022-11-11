07654b08f84c30a60bd089313f4a88ac8bb47e53
NicholasMoon <moonmoon@seas.upenn.edu>
2022-11-10 19:27:25 -0500

    adding more code for wave function collapse

--- FantasyMapGenerator/Assets/Scripts/HexMetrics.cs
+++ FantasyMapGenerator/Assets/Scripts/HexMetrics.cs
@@ -7,6 +7,7 @@ public class HexMetrics : MonoBehaviour
 	public const float outer_radius = 1f;
 	public const float inner_radius = outer_radius * 0.866025404f;
 	public enum NeighborDirections { Left, Right, Upper_Left, Upper_Right, Lower_Left, Lower_Right };
+
     public Dictionary<NeighborDirections, Vector2> neighbor_directions = new Dictionary<NeighborDirections, Vector2>
     {
         { NeighborDirections.Left, new Vector2(-1, 0) },
@@ -16,4 +17,7 @@ public class HexMetrics : MonoBehaviour
         //new Vector2(0, -1),
         //new Vector2(1, -1)
     };
+
+
+    public enum TerrainFeature { Water, Land };
 }
--- FantasyMapGenerator/Assets/Scripts/Tile.cs
+++ FantasyMapGenerator/Assets/Scripts/Tile.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+
+    public int edge_0 = 0;
     // Start is called before the first frame update
     void Start()
     {
